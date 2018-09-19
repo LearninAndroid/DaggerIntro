0c13b2caa8ad158cb20482cc58332919a0c54976
mtotodev05 <mtotodev05@gmail.com>
2018-09-19 12:23:09 +0300

    Inject mystring

--- app/src/main/java/dev/brian/com/myapplication/MainActivity.java
+++ app/src/main/java/dev/brian/com/myapplication/MainActivity.java
@@ -2,12 +2,22 @@ package dev.brian.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    MyString myString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.txtMyString);
+        ((MyApplication)getApplication()).getMyComponent().inject(MainActivity.this);
+
+        textView.setText(myString.myStringVal());
     }
 }
